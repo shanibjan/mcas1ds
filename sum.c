8ebdec0dc27bc46a0cf3b91748883c805d3206e9
shanibjan <100988481+shanibjan@users.noreply.github.com>
2022-03-05 12:08:39 +0530

    final commit

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,s;
+    printf("enter 2 numbers");
+    scanf("%d%d",&n1,&n2);
+    s=n1+n2;
+    printf("%d+%d=%d",n1,n2,s);
+    return 0;
+    
+}
